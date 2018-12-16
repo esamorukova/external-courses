3f260078d095a2ec9702df7bb2b7b97441ab208e
esamorukova <43888761+esamorukova@users.noreply.github.com>
2018-12-16 20:45:06 +0300

    Update classwork.js

--- src/ex15_js-oop/classwork.js
+++ src/ex15_js-oop/classwork.js
@@ -1,69 +1,67 @@
- function Shape() {
+function Shape(type) {
+    this.type = type;
+}
+Shape.prototype.getType = function() {
+    return "Это " + this.type;
+}
+Shape.prototype.getPerimeter = function() {
+    return "";
+}
 
- }
+function Triangle(a, b, c) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    Shape.call(this, "Triangle")
 
 
- Shape.prototype.getType = function() {
-     return "Это "
- }
- this.getPerimeter = function() {
-     return "";
- }
+}
+Triangle.prototype = Object.create(Shape.prototype);;
+Triangle.prototype.constructor = Triangle;
 
 
- function Triangle(a, b, c) {
+Triangle.prototype.getPerimeter = function() {
+    this.perimeter = (this.a + this.b + this.c);
+    return this.perimeter;
+}
 
-     this.a = a;
-     this.b = b;
-     this.c = c;
- }
- Triangle.prototype = Object.create(Shape.prototype);;
- Triangle.prototype.constructor = Triangle;
- Triangle.prototype.getType = function() {
-     return 'Это Triangle'
- }
- Triangle.prototype.getPerimeter = function() {
-     return "Периметр = " + (this.a + this.b + this.c);
- }
 
- function Square(a, b, c, d) {
+function Square(a, b, c, d) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.d = d;
+    Shape.call(this, "Square")
+}
+Square.prototype = Object.create(Shape.prototype);
+Square.prototype.constructor = Square;
 
-     this.a = a;
-     this.b = b;
-     this.c = c;
-     this.d = d;
- }
- Square.prototype = Object.create(Shape.prototype);
- Square.prototype.constructor = Square;
- Square.prototype.getType = function() {
-     return 'Это Square'
- }
- Square.prototype.getPerimeter = function() {
-     return "Периметр = " + (this.a + this.b + this.c + this.d);
- }
 
- function Trapezium(a, b, c, d) {
+Square.prototype.getPerimeter = function() {
+    this.perimeter = (this.a + this.b + this.c + this.d);
+    return this.perimeter;
+}
 
-     this.a = a;
-     this.b = b;
-     this.c = c;
-     this.d = d;
- }
- Trapezium.prototype = Object.create(Square.prototype);
- Trapezium.prototype.constructor = Trapezium;
- Trapezium.prototype.getType = function() {
-     return 'Это Trapezium'
- }
- Trapezium.prototype.getPerimeter = function() {
-     return "Периметр = " + (this.a + this.b + this.c + this.d);
- }
- var triang = new Triangle(4, 6, 7);
- var squar = new Square(1, 2, 3, 4);
- var trap = new Trapezium(2, 3, 4, 5)
+function Trapezium(a, b, c, d) {
+    this.a = a;
+    this.b = b;
+    this.c = c;
+    this.d = d;
+    Shape.call(this, "Trapezium")
+}
+Trapezium.prototype = Object.create(Square.prototype);
+Trapezium.prototype.constructor = Trapezium;
 
- console.log(triang.getType());
- console.log(triang.getPerimeter());
- console.log(squar.getType());
- console.log(squar.getPerimeter());
- console.log(trap.getType());
- console.log(trap.getPerimeter());
+Trapezium.prototype.getPerimeter = function() {
+    this.perimeter = (this.a + this.b + this.c + this.d);
+    return this.perimeter;
+}
+var triang = new Triangle(4, 6, 7);
+var squar = new Square(1, 2, 3, 4);
+var trap = new Trapezium(2, 3, 4, 5)
+console.log(triang.getType());
+console.log('Периметр =' + triang.getPerimeter());
+console.log(squar.getType());
+console.log('Периметр =' + squar.getPerimeter());
+console.log(trap.getType());
+console.log('Периметр =' + trap.getPerimeter());
